b958818bc233e65e24e7ac27f84153d97607d74a
Mochammad Hifzhisyaukani <mochammadhifzhisyaukani@smkwikrama.sch.id>
2026-05-06 12:29:42 +0700

    nambah foto

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,13 @@
 const navbar = document.getElementById('navbar');
 navbar.innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark w-100">
-        <div class="container-sm">
-            <a class="navbar-brand fw-bold text-black" href="/index.html" style="font-size: 20px;">KasWarga</a>
+        <div class="container-sm d-flex justify-content-start">
+            <a class="navbar-brand fw-bold text-black" href="/index.html" style="font-size: 20px;">ScanGo</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon bg-dark rounded-1"></span>
             </button>
+
 
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mx-auto" id="myTab" role="tablist">
@@ -14,7 +15,7 @@ navbar.innerHTML = `
                     <a class="nav-link dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Product
           </a>
-          <ul class="dropdown-menu">
+          <ul class="dropdown-menu">    
             <li><a class="dropdown-item" href="#">Scan Present</a></li>
             <li><a class="dropdown-item" href="#">Dashboard Student</a></li>
             <li><hr class="dropdown-divider"></li>
