4ad186e018635ca220b46827ea49dfc04ff13351
yazid394 <yaziddiansyah093@gmail.com>
2026-05-10 14:16:30 +0700

    quest: Yazid ganteng update index

--- src/page/content/index.js
+++ src/page/content/index.js
@@ -1,19 +1,23 @@
 let content = document.getElementById("content");
 content.innerHTML = `
-    <div class="container text-center">
-                <div class="row">
-                    <div class="col">
-                        <h1>Revolusi Absensi <br>
-                            Siswa jadi Lebih <br>
-                            Mudah & Cepat</h1>
-
-                        <p>Sistem manajemen kehadiran modern berbasis QR Code untuk efisiensi waktu belajar mengajar di sekolah Anda.</p>
-                        <button onclick="#">Get Started</button>
-                        <button onclick="#">Explore</button>
+    <div class="container px-4 text-center">
+        <div class="row gx-5">
+            <div class="col">
+                <div class="p-3">
+                    <h1 style="font-size: 60px; text-align: left; color: white;">Revolusi Absensi Siswa Jadi Lebih Mudah & Cepat</h1>
+                    <p style="text-align: left; color: white;">Sistem manajemen kehadiran modern berbasis QR Code untuk<br> efisiensi waktu belajar mengajar di sekolah Anda.</p>
+                    <div class="button" style="text-align: left; color: white;">
+                        <button style="background-color: blue; color: white;">Get Started</button>
+                        <button>Get Started</button>
                     </div>
-                    <div class="col">
-                        <img src="src/assets/images/GambarAwal.png" alt="GambarScanQR">
-                    </div>
+                    
                 </div>
             </div>
+            <div class="col">
+                <div class="p-3">
+                
+                </div>
+            </div>
+        </div>x
+    </div>
 `;
