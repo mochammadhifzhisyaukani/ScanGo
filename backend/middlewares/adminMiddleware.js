f37478f854c8d6ce72d1636b8d384417bf752a53
Mochammad Hifzhisyaukani <mochammadhifzhisyaukani@smkwikrama.sch.id>
2026-06-15 14:21:24 +0700

    Hifzhi: Swipper

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,4 +1,4 @@
-const verifyAdmin = (req, res, next) => {
+ const verifyAdmin = (req, res, next) => {
   if (!req.user || req.user.role.trim().toLowerCase() !== "admin") {
     showToast("Akses ditolak! Halaman ini khusus untuk Admin.", "danger")
     Swal.fire({
